0b4be72997054bfd5808f83de63d125c3eca1ce9
mariem mkadem <mariem.mkadem@esprit.tn>
2024-04-11 20:37:05 +0100

    first config

---
+++ src/test/java/com/example/amanetpfe/AmanetPfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amanetpfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmanetPfeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
